bfa2de49a787e60d946ed3885a0fe7121da1c0b5
Matias <sanchezmatias968@gmail.com>
2023-11-11 14:59:43 -0300

    pdf-excel-fitros pacienter version1

--- lib/pacientes_doctores/pantalla_principal.dart
+++ lib/pacientes_doctores/pantalla_principal.dart
@@ -2,6 +2,10 @@ import 'package:flutter/material.dart';
 import 'package:frontend_2da_parcial/pacientes_doctores/model.dart';
 import 'package:frontend_2da_parcial/pacientes_doctores/actions.dart';
 import 'package:frontend_2da_parcial/pacientes_doctores/pantalla_agregar.dart';
+import 'package:pdf/widgets.dart' as pw;
+import 'package:path_provider/path_provider.dart';
+import 'package:excel/excel.dart';
+import 'dart:io';
 
 class AdministracionPacientesDoctoresScreen extends StatefulWidget {
   @override
@@ -12,6 +16,8 @@ class AdministracionPacientesDoctoresScreen extends StatefulWidget {
 class _AdministracionPacientesDoctoresScreenState
     extends State<AdministracionPacientesDoctoresScreen> {
   List<PacienteDoctor> pacientesDoctores = [];
+  List<PacienteDoctor> pacientesDoctoresFiltrados = [];
+  String selectedFilter = 'Nombre';
 
   @override
   void initState() {
@@ -24,6 +30,7 @@ class _AdministracionPacientesDoctoresScreenState
         await PacienteDoctorDatabaseProvider().getAllPacientesDoctores();
     setState(() {
       pacientesDoctores = listaPacientesDoctores;
+      pacientesDoctoresFiltrados = listaPacientesDoctores;
     });
   }
 
@@ -37,7 +44,8 @@ class _AdministracionPacientesDoctoresScreenState
         cedula: 'Nueva Cédula',
         flagEsDoctor: 1); // Ajusta los valores según tu caso
 
-    await PacienteDoctorDatabaseProvider().insertPacienteDoctor(nuevoPacienteDoctor);
+    await PacienteDoctorDatabaseProvider()
+        .insertPacienteDoctor(nuevoPacienteDoctor);
     getPacientesDoctores();
   }
 
@@ -53,6 +61,108 @@ class _AdministracionPacientesDoctoresScreenState
     }
   }
 
+  void filterCategorias(String searchText) {
+    setState(() {
+      pacientesDoctoresFiltrados = pacientesDoctores.where((pacientesDoctores) {
+        if (selectedFilter == 'Nombre') {
+          return pacientesDoctores.nombre
+              .toLowerCase()
+              .contains(searchText.toLowerCase());
+        } else if (selectedFilter == 'Apellido') {
+          return pacientesDoctores.apellido
+              .toLowerCase()
+              .contains(searchText.toLowerCase());
+        } else {
+          return pacientesDoctores.idPersona != null &&
+              pacientesDoctores.idPersona.toString() == searchText;
+        }
+      }).toList();
+    });
+  }
+
+  void exportToExcel(List<PacienteDoctor> data) async {
+    final excel = Excel.createExcel();
+    final sheet = excel['Listado de Pacientes y Doctores'];
+
+    // Agrega encabezados
+    sheet.appendRow(['ID', 'Nombre']);
+
+    // Agrega tus datos
+    for (final paciente_doctor in data) {
+      sheet.appendRow([paciente_doctor.idPersona, paciente_doctor.nombre]);
+    }
+
+    // Guarda el archivo Excel en el directorio de documentos
+    final directory = await getApplicationDocumentsDirectory();
+    final fileName = 'paciente_doctor.xlsx';
+    final downloadsDirectory = '/storage/emulated/0/Download';
+    final filePath = '$downloadsDirectory/$fileName';
+
+    final excelData = await excel.encode(); // Espera la finalización del Future
+
+    if (excelData != null) {
+      await File(filePath).writeAsBytes(excelData);
+    } else {
+      print('Error al exportar a Excel'); // Manejo de errores
+    }
+  }
+
+  void exportToPDF(List<PacienteDoctor> data) async {
+    final pdf = pw.Document();
+    // Agrega un título
+    pdf.addPage(
+      pw.Page(
+        build: (context) {
+          return pw.Center(
+            child: pw.Text('Listado de Pacientes y Doctores',
+                style: pw.TextStyle(fontSize: 24)),
+          );
+        },
+      ),
+    );
+
+    // Agrega tus datos
+    for (final paciente_doctor in data) {
+      pdf.addPage(
+        pw.Page(
+          build: (context) {
+            return pw.Column(
+              children: [
+                pw.Text('ID: ${paciente_doctor.idPersona}'),
+                pw.Text('Descripción: ${paciente_doctor.nombre}'),
+                pw.Divider(),
+              ],
+            );
+          },
+        ),
+      );
+    }
+
+    // Guarda el archivo PDF en el directorio de documentos
+    final directory = await getApplicationDocumentsDirectory();
+    // final filePath = '${directory.path}/categorias.pdf';
+    final fileName = 'paciente_doctor.pdf';
+
+    final downloadsDirectory = '/storage/emulated/0/Download';
+    final filePath = '$downloadsDirectory/$fileName';
+
+    final file = File(filePath);
+    print(filePath);
+
+    // Espera a que el Future se complete y obtén los datos del archivo PDF
+    final pdfData = await pdf.save();
+
+    if (pdfData != null) {
+      // Convierte los datos del archivo PDF en una lista de enteros
+      final pdfBytes = pdfData.buffer.asUint8List();
+
+      // Escribe los datos en el archivo
+      await file.writeAsBytes(pdfBytes);
+    } else {
+      print('Error al exportar a PDF'); // Manejo de errores
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -63,6 +173,28 @@ class _AdministracionPacientesDoctoresScreenState
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
+            Row(
+              children: [
+                Text('Filtrar por:'),
+                DropdownButton<String>(
+                  value: selectedFilter,
+                  onChanged: (String? newValue) {
+                    if (newValue != null) {
+                      setState(() {
+                        selectedFilter = newValue;
+                      });
+                    }
+                  },
+                  items: <String>['Nombre', 'Apellido']
+                      .map<DropdownMenuItem<String>>((String value) {
+                    return DropdownMenuItem<String>(
+                      value: value,
+                      child: Text(value),
+                    );
+                  }).toList(),
+                ),
+              ],
+            ),
             ElevatedButton(
               onPressed: () {
                 Navigator.of(context).push(MaterialPageRoute(builder: (_) {
@@ -74,14 +206,39 @@ class _AdministracionPacientesDoctoresScreenState
                 minimumSize: MaterialStateProperty.all(Size(200, 48)),
               ),
             ),
+            TextField(
+              onChanged: (value) {
+                filterCategorias(value);
+              },
+              decoration: InputDecoration(labelText: 'Buscar'),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                ElevatedButton(
+                  onPressed: () {
+                    exportToExcel(pacientesDoctoresFiltrados);
+                  },
+                  child: Text('Exportar a Excel'),
+                ),
+                SizedBox(width: 16),
+                ElevatedButton(
+                  onPressed: () {
+                    exportToPDF(pacientesDoctoresFiltrados);
+                  },
+                  child: Text('Exportar a PDF'),
+                ),
+              ],
+            ),
             // Aquí puedes mostrar la lista de pacientes y doctores.
             ListView.builder(
               shrinkWrap: true,
-              itemCount: pacientesDoctores.length,
+              itemCount: pacientesDoctoresFiltrados.length,
               itemBuilder: (context, index) {
-                final pacienteDoctor = pacientesDoctores[index];
+                final pacienteDoctor = pacientesDoctoresFiltrados[index];
                 return ListTile(
-                  title: Text('${pacienteDoctor.nombre} ${pacienteDoctor.apellido}'),
+                  title: Text(
+                      '${pacienteDoctor.nombre} ${pacienteDoctor.apellido}'),
                   trailing: Row(
                     mainAxisSize: MainAxisSize.min,
                     children: [
